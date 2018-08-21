78f35f5c523c9c911c9dcd32dc628d2d82692c40
tdenton <tdenton@localhost>
2018-08-20 19:57:55 -0600

    Added class structure for AbstractPart, Product and Inventory.

---
+++ src/main/java/com/mycompany/mavenproject1/Part.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+
+/**
+ *
+ * @author tdenton
+ */
+abstract class Part {
+
+    private int partID;
+    private String name = new String();
+    private Double price;
+    private int inStock;
+    private int min;
+    private int max;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    ;
+    
+    public String getName() {
+        return this.name;
+    }
+
+    ;
+    
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setInStock(int inStock) {
+        this.inStock = inStock;
+    }
+
+    public int getInStock() {
+        return this.inStock;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMin() {
+        return this.min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMax() {
+        return this.max;
+    }
+
+    public void setPartID(int partID) {
+        this.partID = partID;
+    }
+
+    public int getPartID() {
+        return this.partID;
+    }
+}
+
+class InHouse extends Part {
+    private int machineID;
+    
+    public void setMachineID(int machineID){
+        this.machineID = machineID;
+    }
+    
+    public int getMachineID(){
+        return this.machineID;
+    }
+}
+
+class Outsourced extends Part {
+    String companyName = new String();
+    
+    public void setCompanyName(String companyName){
+        this.companyName = companyName;
+    }
+    
+    public String getCompanyName(){
+        return this.companyName;
+    }
+}
---
+++ src/main/java/com/mycompany/mavenproject1/Product.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author tdenton
+ */
+public class Product {
+    private List<Part> associatedParts = new ArrayList<>();
+    private int productID;
+    private String name = new String();
+    private Double price;
+    private int inStock;
+    private int min;
+    private int max;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setPrice(Double price){
+        this.price = price;
+    }
+    
+    public Double getPrice(){
+        return this.price;
+    }
+    
+    public void setInStock(int inStock){
+        this.inStock = inStock;
+    }
+    
+    public int getInStock(){
+        return this.inStock;
+    }
+    
+    public void setMin(int min){
+        this.min = min;
+    }
+    
+    public int getMin(){
+        return this.min;
+    }
+    
+    public void setMax(int max){
+        this.max = max;
+    }
+    
+    public int getMax(){
+        return this.max;
+    }
+    
+    public void addAssociatedPart(Part part){
+        
+    }
+    
+    public boolean removeAssociatedPart(int index){
+        return true;
+    }
+    
+    public Part lookupAssociatedPart(int index){
+        Part part = new Outsourced();
+        return part;
+    }
+    
+    public void setProductID(int productID){
+        this.productID = productID;
+    }
+    
+    public int getProductID(){
+        return this.productID;
+    }
+}
