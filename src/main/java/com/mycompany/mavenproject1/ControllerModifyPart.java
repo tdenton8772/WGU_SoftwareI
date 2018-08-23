ff11ca3713e4e6a1498f7abb7bc639ac1849a0d1
tdenton <tdenton@localhost>
2018-08-23 13:50:05 -0600

    Added DataValidation and alert for modify part

--- src/main/java/com/mycompany/mavenproject1/ControllerModifyPart.java
+++ src/main/java/com/mycompany/mavenproject1/ControllerModifyPart.java
@@ -13,6 +13,8 @@ import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -80,39 +82,128 @@ public class ControllerModifyPart implements Initializable {
 
     }
 
+    public class ValidateReturn {
+        boolean validated = true;
+        String messageCode = "";
+    }
+
+    private ValidateReturn ValidatePart(String name, String inv, String price, String min, String max, Class classs, String machComp) {
+        ValidateReturn object = new ValidateReturn();
+        if (name.equals("")) {
+            object.validated = false;
+            object.messageCode = "You must include a Name";
+            return object;
+        }
+        
+        try {
+            Integer.parseInt(inv);
+        } catch (Exception ex){
+            object.validated = false;
+            object.messageCode = "Current inventory must be an integer";
+            return object;
+        }
+
+        try {
+            Integer.parseInt(min);
+        } catch (Exception ex){
+            object.validated = false;
+            object.messageCode = "Minimum inventory must be an integer";
+            return object;
+        }
+        
+        try {
+            Integer.parseInt(max);
+        } catch (Exception ex){
+            object.validated = false;
+            object.messageCode = "Maximum inventory must be an integer";
+            return object;
+        }
+        
+        try {
+            Double.parseDouble(price);
+        } catch (Exception ex){
+            object.validated = false;
+            object.messageCode = "Price must be a double";
+            return object;
+        }
+        
+        if (Integer.parseInt(min) > Integer.parseInt(max)){
+            object.validated = false;
+            object.messageCode = "Minimum integer values must be less than or equal to maximum integer values";
+            return object;
+        }
+        
+        if(Integer.parseInt(inv) < Integer.parseInt(min) || Integer.parseInt(inv) > Integer.parseInt(max)){
+            object.validated = false;
+            object.messageCode = "Inventory must be between or equal to min and max inventory values";
+            return object;
+        }
+        
+        if(classs.equals(InHouse.class)){
+            try{
+                Integer.parseInt(machComp);
+            } catch (Exception ex){
+                object.validated = false;
+                object.messageCode = "Must have a valid Machine ID";
+                return object;
+            }
+        }else if(classs.equals(Outsourced.class)){
+            if(machComp.equals("")){
+                object.validated = false;
+                object.messageCode = "You must have a valid company name";
+                return object;
+            }
+        }
+        
+        return object;
+    }
+
     @FXML
     protected void handleModifyPartSave(ActionEvent event) {
-        try {
-            String part_Name = txt_partName.getText();
-            int part_Inv = Integer.parseInt(txt_partInv.getText());
-            Double part_Price = Double.parseDouble(txt_partPrice.getText());
-            int part_Min = Integer.parseInt(txt_partMin.getText());
-            int part_Max = Integer.parseInt(txt_partMax.getText());
-   
-            Inventory inventory = Inventory.getInstance();
-            int part_ID = Integer.parseInt(txt_partID.getText());
+        ValidateReturn returnObject = ValidatePart(txt_partName.getText(), txt_partInv.getText(), txt_partPrice.getText(), txt_partMin.getText(), txt_partMax.getText(), part.getClass(), txt_partMachComp.getText());
+
+        if (returnObject.validated == false) {
+            System.out.println(returnObject.messageCode);
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Modify Part Error");
+            alert.setHeaderText("Data Validation Error");
+            alert.setContentText(returnObject.messageCode);
+            alert.showAndWait();
             
-            String opt_selected = ((RadioButton) opt_source.getSelectedToggle()).getText();
+        } else {
+            try {
 
-            if (opt_selected.equalsIgnoreCase("In-House")) {
-                int part_MachineID = Integer.parseInt(txt_partMachComp.getText());
-                InHouse part = new InHouse(part_ID, part_Name, part_Price, part_Inv, part_Min, part_Max, part_MachineID);
-                inventory.updatePart(part);
+                String part_Name = txt_partName.getText();
+                int part_Inv = Integer.parseInt(txt_partInv.getText());
+                Double part_Price = Double.parseDouble(txt_partPrice.getText());
+                int part_Min = Integer.parseInt(txt_partMin.getText());
+                int part_Max = Integer.parseInt(txt_partMax.getText());
 
-            } else if (opt_selected.equalsIgnoreCase("OutSourced")) {
-                String part_CompName = txt_partMachComp.getText();
-                Outsourced part = new Outsourced(part_ID, part_Name, part_Price, part_Inv, part_Min, part_Max, part_CompName);
-                inventory.updatePart(part);
-            } else {
+                Inventory inventory = Inventory.getInstance();
+                int part_ID = Integer.parseInt(txt_partID.getText());
 
+                String opt_selected = ((RadioButton) opt_source.getSelectedToggle()).getText();
+
+                if (opt_selected.equalsIgnoreCase("In-House")) {
+                    int part_MachineID = Integer.parseInt(txt_partMachComp.getText());
+                    InHouse part = new InHouse(part_ID, part_Name, part_Price, part_Inv, part_Min, part_Max, part_MachineID);
+                    inventory.updatePart(part);
+
+                } else if (opt_selected.equalsIgnoreCase("OutSourced")) {
+                    String part_CompName = txt_partMachComp.getText();
+                    Outsourced part = new Outsourced(part_ID, part_Name, part_Price, part_Inv, part_Min, part_Max, part_CompName);
+                    inventory.updatePart(part);
+                } else {
+
+                }
+
+                System.out.println("Modify Part: Save was pushed");
+                Stage stage = (Stage) cancelButton.getScene().getWindow();
+                stage.close();
+                new GetMainStage();
+            } catch (Exception ex) {
+                System.out.println("Error: Modify Part Save was pushed");
             }
-            
-            System.out.println("Modify Part: Save was pushed");
-            Stage stage = (Stage) cancelButton.getScene().getWindow();
-            stage.close();
-            new GetMainStage();
-        } catch (Exception ex) {
-            System.out.println("Error: Modify Part Save was pushed");
         }
     }
 
@@ -130,7 +221,7 @@ public class ControllerModifyPart implements Initializable {
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {        
+    public void initialize(URL url, ResourceBundle rb) {
         root.sceneProperty().addListener((observable, oldValue, newValue) -> {
             if (this.part.getClass().equals(InHouse.class)) {
                 System.out.println("InHouse");
@@ -139,7 +230,7 @@ public class ControllerModifyPart implements Initializable {
                 opt_OutSourced.setSelected(false);
                 lbl_partCompanyName.setVisible(false);
                 lbl_partMachineID.setVisible(true);
-                
+
                 txt_partID.setText(Integer.toString(InHousePart.getPartID()));
                 txt_partName.setText(InHousePart.getName());
                 txt_partInv.setText(Integer.toString(InHousePart.getInStock()));
@@ -161,13 +252,12 @@ public class ControllerModifyPart implements Initializable {
                 txt_partMin.setText(Integer.toString(OutSourcedPart.getMin()));
                 txt_partMax.setText(Integer.toString(OutSourcedPart.getMax()));
                 txt_partMachComp.setText(OutSourcedPart.getCompanyName());
-                
+
             } else {
                 System.out.println("Undefined Class in Parts list" + part.getClass());
             }
-            
-            
+
         });
-        
+
     }
 }
